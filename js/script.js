2535578ad6af2ab76804fe9a96a86a84032f61b0
Igor_Polyakov <itworldigpo@gmail.com>
2023-09-19 17:25:06 +0300

    change links

--- js/script.js
+++ js/script.js
@@ -3,17 +3,16 @@ const reviewsSwiper = document.querySelector('.swiper-reviews');
 
 if(reviewsSwiper){
     const swiper = new Swiper('.swiper-reviews', {
-        // Optional parameters
         autoHeight: true, 
         loop: true,
     
-        // If we need pagination
         pagination: {
         el: '.swiper-pagination',
         clickable: true
         },
     });
 }
+
 
 let useToScroll = document.querySelector ('.scroll-menu__link');
 

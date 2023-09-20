c0c8257d9aa76e15e241424bf7a247d7c90a8200
Igor_Polyakov <itworldigpo@gmail.com>
2023-09-20 14:22:11 +0300

    add heder categories in categories

--- js/categories.js
+++ js/categories.js
@@ -6,11 +6,34 @@ mainInfo.forEach (buttonEvent => {
         if (event.target.closest('.categori__image')){
             buttonEvent.classList.toggle ('_active')
         }
-
         if (!event.target.closest ('.categori__image') ){
             buttonEvent.classList.remove ('_active')
         }  
-
+    })
+    document.addEventListener ('click', function(event){
+        if (!event.target.closest('.categori__image')){
+            buttonEvent.classList.remove ('_active')
+        }
     }) 
 })
+
+let mainCategories = document.querySelectorAll ('.menu__item-button')
+
+
+mainCategories.forEach (buttonCategori => {
+    buttonCategori.addEventListener ('click' , function (event){
+        if (event.target.closest ('.menu__item-button')){
+            buttonCategori.classList.toggle ('_showcategori')
+        }
+        if (!event.target.closest ('.menu__item-button')){
+            buttonCategori.classList.remove ('_showcategori')
+        }
+    })
+    document.addEventListener ('click', function(event){
+        if (!event.target.closest('.menu__item-button')){
+            buttonCategori.classList.remove ('_showcategori')
+        }
+    })
+})
+
 
